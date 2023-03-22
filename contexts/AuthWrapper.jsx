d34ef79831b0d4d553f6d0b874d52c471e9f4f4b
jhyunwoo <jhyunwoo0228@gmail.com>
2023-03-22 13:53:44 +0900

    add kakao login

---
+++ contexts/AuthWrapper.jsx
@@ -0,0 +1,92 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import pb from "../lib/pocketbase";
+
+const AuthContext = createContext(null);
+
+const AuthWrapper = ({ children }) => {
+  const router = useRouter();
+
+  const [user, setUser] = useState(null);
+  const [googleAuthProvider, setGoogleAuthProvider] = useState(null);
+  const [githubAuthProvider, setGithubAuthProvider] = useState(null);
+  const [kakaoAuthProvider, setKakaoAuthProvider] = useState(null);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      const authMethods = await pb
+        .collection("users")
+        .listAuthMethods()
+        .then((methods) => methods)
+        .catch((err) => {
+          console.error(err);
+        });
+
+      if (authMethods)
+        for (const provider of authMethods.authProviders) {
+          if (provider.name === "google") setGoogleAuthProvider(provider);
+          if (provider.name === "github") setGithubAuthProvider(provider);
+          if (provider.name === "kakao") setKakaoAuthProvider(provider);
+        }
+    };
+
+    initAuth();
+
+    if (pb.authStore.model) setUserData(pb.authStore.model);
+  }, []);
+
+  const setUserData = (pbUser) => {
+    const { id, name, email, username, avatarUrl } = pbUser;
+    setUser({ id, name, email, username, avatarUrl });
+  };
+
+  const googleSignIn = () => {
+    signOut();
+    localStorage.setItem("provider", JSON.stringify(googleAuthProvider));
+    const redirectUrl = `${location.origin}/signin`;
+    const url = googleAuthProvider?.authUrl + redirectUrl;
+
+    router.push(url);
+  };
+
+  const githubSignIn = () => {
+    signOut();
+    localStorage.setItem("provider", JSON.stringify(githubAuthProvider));
+    const redirectUrl = `${location.origin}/signin`;
+    const url = githubAuthProvider?.authUrl + redirectUrl;
+
+    router.push(url);
+  };
+
+  const kakaoSignIn = () => {
+    signOut();
+    localStorage.setItem("provider", JSON.stringify(kakaoAuthProvider));
+    const redirectUrl = `${location.origin}/signin`;
+    const url = kakaoAuthProvider?.authUrl + redirectUrl;
+
+    router.push(url);
+  };
+
+  const signOut = () => {
+    setUser(null);
+    pb.authStore.clear();
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        googleSignIn,
+        githubSignIn,
+        setUserData,
+        signOut,
+        kakaoSignIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const usePbAuth = () => useContext(AuthContext);
+export default AuthWrapper;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,25 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { usePbAuth } from "../contexts/AuthWrapper";
+
 export default function Home() {
+  const { user, signOut } = usePbAuth();
+  const router = useRouter();
+
+  function checkUserAuth() {
+    if (!user) {
+      router.replace("/signin");
+    }
+  }
+
+  useEffect(() => checkUserAuth(), [user]);
+
   return (
     <div>
       <div className="w-full h-screen flex justify-center items-center text-4xl font-extrabold">
         DEARU Home Page
       </div>
+      <button onClick={signOut}>Sign Out</button>
     </div>
   );
 }
