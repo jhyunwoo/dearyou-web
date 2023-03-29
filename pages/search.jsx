88b00f4dc79e9e32be85bdfdd3c8498698a09496
연준 김 <kyjun050803@gmail.com>
2023-03-29 17:23:09 +0900

    검색 결과창에 상품 이미지 추가

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,33 +1,51 @@
-import { useState, useRef } from "react";
-import pb from "../lib/pocketbase";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState, useRef } from "react";
+import { usePbAuth } from "../contexts/AuthWrapper";
+import pb from "@/lib/pocketbase";
 
 export default function Search() {
-  const [itemData, setItemData] = useState([]);
+  const { user, signOut } = usePbAuth();
+  const [products, setProducts] = useState([]);
+  const [searched, setSearched] = useState([]);
   const searchInput = useRef("");
+  const router = useRouter();
+
+  function checkUserAuth() {
+    if (!user) {
+      router.replace("/signin");
+    }
+  }
+
+  // PocketBase에서 products(상품 정보) 컬렉션 가져옴
+  async function getProductsLists() {
+    const resultList = await pb
+      .collection("products")
+      .getList(1, 50, { expand: "seller" });
+    console.log(resultList.items);
+    setProducts(resultList?.items);
+  }
 
   async function handleSearch(event) {
     event.preventDefault();
     let searchWord = searchInput.current.value.toLowerCase(); // 검색어
     if(searchWord.length === 0) return;
 
-    // PocketBase에서 products(상품 정보) 컬렉션 가져옴
-    let temp = await pb.collection("products").getFullList({
-      sort: "-created",
-    });
-
     // 'searchWord'(검색어)가 포함된 상품만 data 리스트에 저장
     let data = [];
-    for (let i = 0; i < temp.length; i++) {
+    for (let i = 0; i < products.length; i++) {
       if (
-        (temp[i].name).toLowerCase().indexOf(searchWord) >= 0 ||
-        (temp[i].explain).toLowerCase().indexOf(searchWord) >= 0
+        (products[i]?.name).toLowerCase().indexOf(searchWord) >= 0 ||
+        (products[i]?.explain).toLowerCase().indexOf(searchWord) >= 0 ||
+        (products[i]?.expand?.seller?.name).toLowerCase().indexOf(searchWord) >= 0
       ) {
-        data.push(temp[i]);
+        data.push(products[i]);
       }
     }
 
-    // 화면에 표시할 정보만을 담은 'itemData' state 설정
-    setItemData(data);
+    // 화면에 표시할 정보만을 담은 'searched' state 설정
+    setSearched(data);
   }
 
   // SearchBar -> 'searchQuery' state에 검색어 저장
@@ -39,7 +57,7 @@ export default function Search() {
           type="text"
           placeholder="검색어를 입력하세요..."
         />
-        <button type="submit">Search</button>
+        <button type="submit">검색</button>
       </form>
     );
   }
@@ -48,33 +66,43 @@ export default function Search() {
   function ItemList(props) {
     if(props.data.length === 0){ // props로 전달받은 검색 결과 목록이 비었을 때
       return (
-        <h3>검색 결과가 없습니다.</h3>
+        <h3 className="text-2xl font-bold">검색 결과가 없습니다.</h3>
       )
     }
     else{ // 검색 결과 표시하는 Ordered List
       return (
         <div>
-          <h3>검색 결과</h3>
-          <ol>
-            {props.data.map((item) => (
-              <li key={item.id}>
-                <hr />
-                <div>{item.name}</div>
-                <div>{item.explain}</div>
-                <div>{item.created}</div>
+          <h3 className="text-2xl font-bold">검색 결과</h3>
+          <ul>
+            {searched.map((data, key) => (
+              <li key={key}>
+                <hr/>
+                <div>{data?.name}</div>
+                <div>{data?.explain}</div>
+                <div>등록인: {data?.expand?.seller?.name}</div>
+                <Image
+                  src={`https://dearu-pocket.moveto.kr/api/files/products/${data.id}/${data.photos[0]}`}
+                  width={500}
+                  height={500}
+                ></Image>
               </li>
             ))}
-          </ol>
+          </ul>
         </div>
       );
     }
   }
 
+  useEffect(() => checkUserAuth(), [user]);
+  useEffect(() => {
+    getProductsLists();
+  }, []);
+
   return (
     <div>
-      <div>Search Page</div>
+      <h1>Search Page</h1>
       <SearchBar />
-      <ItemList data={itemData} />
+      <ItemList data={searched} />
     </div>
   );
 }
