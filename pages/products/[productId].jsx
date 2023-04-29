fc60157d1c7284d17ed55bf791922aed92fd5fac
전현우 <jhyunwoo@moveto.local>
2023-04-29 23:14:06 +0900

    fix coflict

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,8 +1,10 @@
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import pb from "@/lib/pocketbase";
 import Image from "next/image";
 import { usePbAuth } from "@/contexts/AuthWrapper";
+import ProtectedPage from "@/components/ProtectedPage";
+import BottomBar from "@/components/BottomBar";
+import { useRef } from "react";
 
 export const getServerSideProps = async (context) => {
   const { query } = context;
@@ -17,7 +19,27 @@ export const getServerSideProps = async (context) => {
 
 export default function ProductDetail({ productId }) {
   const [productInfo, setProductInfo] = useState("");
+  const useWindowSize = () => {
+    const isClient = typeof window === "object";
 
+    const getSize = () => {
+      return { width: isClient ? window.innerWidth : undefined };
+    };
+
+    const [windowSize, setWindowSize] = useState(getSize);
+
+    useEffect(() => {
+      if (!isClient) {
+        return false;
+      }
+      const handleResize = () => {
+        setWindowSize(getSize());
+      };
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    return windowSize;
+  };
   function getUploadedTime(time) {
     let uploadedTime = Date.parse(time);
     let currentTime = new Date().getTime();
@@ -40,7 +62,8 @@ export default function ProductDetail({ productId }) {
       return `${Math.floor(gap < 1000 * 60 * 60 * 24 * 365)}년 전`;
     }
   }
-
+  const windows = useWindowSize();
+  console.log(windows.width);
   useEffect(() => {
     async function getProductInfo() {
       const record = await pb.collection("products").getOne(productId, {
@@ -52,39 +75,44 @@ export default function ProductDetail({ productId }) {
     getProductInfo();
   }, []);
 
-
   //현재 사용자의 wishes에 product를 추가하는 버튼의 함수
   const currentUser = usePbAuth().user;
   async function addToWishlist() {
     try {
       const originWishes = await pb.collection("users").getOne(currentUser.id, {
         expand: "wishes",
-      })
-      const updatedUser = await pb.collection('users').update(currentUser.id, {
-        "wishes": [...originWishes.wishes, productId],
+      });
+      const updatedUser = await pb.collection("users").update(currentUser.id, {
+        wishes: [...originWishes.wishes, productId],
       });
       console.log(updatedUser);
-      alert('Product has been added to your wishlist!');
+      alert("Product has been added to your wishlist!");
     } catch (error) {
-      console.error('Error adding product to wishlist:', error);
-      alert('Error adding product to your wishlist. Please try again later.');
+      console.error("Error adding product to wishlist:", error);
+      alert("Error adding product to your wishlist. Please try again later.");
     }
   }
   return (
-    <div>
-      <div>
+    <ProtectedPage>
+      <BottomBar />
+      <div className="w-full min-h-screen bg-slate-50">
         {productInfo ? (
           <div>
-            <div>
+            <div className="flex overflow-x-auto space-x-8 scrollbar-hide">
               {productInfo.photos.map((data, key) => (
-                <Image
-                  key={key}
-                  src={`https://dearu-pocket.moveto.kr/api/files/products/${productId}/${data}`}
-                  width={500}
-                  height={500}
-                  priority={true}
-                  alt={"Product Image"}
-                />
+                <div
+                  className={`w-[${windows.width}px] h-[${windows.width}px] bg-slate-300 flex-shrink-0`}
+                >
+                  <Image
+                    key={key}
+                    src={`https://dearu-pocket.moveto.kr/api/files/products/${productId}/${data}`}
+                    width={300}
+                    height={300}
+                    priority={true}
+                    alt={"Product Image"}
+                    className=""
+                  />
+                </div>
               ))}
             </div>
             <div>{productInfo.name}</div>
@@ -100,10 +128,8 @@ export default function ProductDetail({ productId }) {
         ) : (
           ""
         )}
-        <button onClick={addToWishlist}>
-          위시리스트에 넣기
-        </button>
+        <button onClick={addToWishlist}>위시리스트에 넣기</button>
       </div>
-    </div>
+    </ProtectedPage>
   );
 }
