7ef0ed8f8cfb8689aebca0d342a370d14fb4f98f
jhyunwoo <jhyunwoo0228@gmail.com>
2023-05-11 10:29:30 +0900

    add larger scale screen ui error fix

--- pages/profile/my-products.jsx
+++ pages/profile/my-products.jsx
@@ -21,7 +21,7 @@ export default function MyProducts() {
       <BottomBar />
       <div className="w-full min-h-screen bg-slate-50 p-4">
         <div className="text-xl font-bold">내 상품</div>
-        <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 gap-2">
           {products?.length === 0 ? (
             <div className="mx-auto mt-24 flex flex-col items-center justify-center">
               <div className="font-semibold">아직 등록한 상품이 없습니다.</div>
@@ -37,22 +37,27 @@ export default function MyProducts() {
           )}
           {products.map((data, key) => (
             <Link href={`/products/${data.id}`} key={key}>
-              <div className="flex flex-row w-full border-b py-3 border-slate-300">
+              <div
+                className={`flex rounded-lg p-2  w-full ${
+                  data?.soldDate ? "bg-slate-100" : "bg-white"
+                }`}
+              >
                 <Image
                   src={`https://dearu-pocket.moveto.kr/api/files/products/${data.id}/${data.photos[0]}?thumb=100x100`}
                   width={300}
                   height={300}
                   alt={data.name}
                   priority={true}
-                  className="basis-1/4 w-24 h-24 rounded-lg mr-4"
+                  className=" w-28 h-28  mr-4 rounded-lg"
                 />
-                <div>
+                <div className="flex justify-between flex-col">
                   <div className="font-bold text-lg">{data?.name}</div>
                   <div className="font-medium text-base flex flex-col">
                     <div className="font-semibold">
                       {data?.expand?.seller?.name}
                     </div>
                     <div>{data?.expand?.seller?.studentId}</div>
+                    <div>{data?.soldDate ? "나눔 완료" : ""}</div>
                   </div>
                 </div>
               </div>
--- pages/search.jsx
+++ pages/search.jsx
@@ -61,26 +61,30 @@ export default function Search() {
       return (
         <div>
           <BottomBar />
-          <div className="grid grid-cols-1 sm:grid-cols-2 p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-2">
             {searched.map((data, key) => (
               <Link href={`/products/${data.id}`} key={key}>
-              <div className={`flex flex-row w-full border-b py-3 border-slate-300 ${data?.soldDate ? "bg-slate-200" : ""}`}>
+                <div
+                  className={`flex rounded-lg p-2  w-full ${
+                    data?.soldDate ? "bg-slate-100" : "bg-white"
+                  }`}
+                >
                   <Image
                     src={`https://dearu-pocket.moveto.kr/api/files/products/${data.id}/${data.photos[0]}?thumb=100x100`}
                     width={300}
                     height={300}
                     alt={data.name}
                     priority={true}
-                    className="basis-1/4 w-24 h-24 rounded-lg mr-4"
+                    className=" w-28 h-28  mr-4 rounded-lg"
                   />
-                  <div>
+                  <div className="flex justify-between flex-col">
                     <div className="font-bold text-lg">{data?.name}</div>
                     <div className="font-medium text-base flex flex-col">
                       <div className="font-semibold">
                         {data?.expand?.seller?.name}
                       </div>
                       <div>{data?.expand?.seller?.studentId}</div>
-                      {data?.soldDate ? "나눔 완료" : ""}
+                      <div>{data?.soldDate ? "나눔 완료" : ""}</div>
                     </div>
                   </div>
                 </div>
