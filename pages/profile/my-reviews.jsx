617a43a7b69e72fc5d3eedb0d9e0a2b7f31cc5a0
연준 김 <kyjun050803@gmail.com>
2023-07-04 11:03:18 +0900

    edit my review page, fix words

--- pages/profile/my-reviews.jsx
+++ pages/profile/my-reviews.jsx
@@ -6,35 +6,61 @@ import { useEffect, useState } from "react";
 import pb from "@/lib/pocketbase";
 
 export default function MyReviews() {
-  const [reviews, setReviews] = useState([]);
+  const [reviewsTo, setReviewsTo] = useState([]);
+  const [reviewsFrom, setReviewsFrom] = useState([]);
 
   useEffect(() => {
     async function getMyReviews() {
-      const reviews = await pb.collection("reviews").getFullList({
+      let reviews = await pb.collection("reviews").getFullList({
         filter: `to.id="${pb.authStore.model?.id}"`,
         expand: "from",
       });
-      setReviews(reviews);
+      setReviewsFrom(reviews);
+      reviews = await pb.collection("reviews").getFullList({
+        filter: `from.id="${pb.authStore.model?.id}"`,
+        expand: "to",
+      });
+      setReviewsTo(reviews);
     }
     getMyReviews();
   }, []);
 
   return (
     <ProtectedPage>
-      <HeadBar title="내 거래 후기" />
+      <HeadBar title="내 나눔 후기" />
       <BottomBar />
       <Layout>
-        <div className="w-full grid grid-cols-1 gap-2">
-          {reviews.map((data, key) => (
+        <div className="mt-2 w-full grid grid-cols-1 gap-2">
+          <div className="text-lg">내가 받은 후기</div>
+          {reviewsFrom.map((data, key) => (
             <div
               key={key}
               className="flex flex-col bg-white p-2 rounded-md shadow-md"
             >
-              <div className="font-base text-slate-700">
-                {data?.expand?.from?.name}
+              <div className="font-base text-slate-700 ml-2">
+                from. {data?.expand?.from?.name}
               </div>
               <div className="mt-1 text-lg font-semibold flex">
                 <div className="text-white bg-amber-400 px-2 rounded-full mx-2">
+                  {data?.rate}
+                </div>
+                <div>{data.comment}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="mt-4 w-full grid grid-cols-1 gap-2">
+          <div className="text-lg">내가 남긴 후기</div>
+          {reviewsTo.map((data, key) => (
+            <div
+              key={key}
+              className="flex flex-col bg-white p-2 rounded-md shadow-md"
+            >
+              <div className="font-base text-slate-700 ml-2">
+                to. {data?.expand?.to?.name}
+              </div>
+              <div className="mt-1 text-lg font-semibold flex">
+                <div className="text-white bg-amber-400 px-2 rounded-lg mx-2">
                   {data?.rate}
                 </div>
                 <div>{data.comment}</div>
