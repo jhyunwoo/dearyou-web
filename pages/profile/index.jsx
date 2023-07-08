6adc423828b730ce8ed1142a984eeb1fd93ea5cd
jhyunwoo <jhyunwoo0228@gmail.com>
2023-07-08 17:31:54 +0900

    fix dark mode

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,7 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 import pb from "@/lib/pocketbase"
-import { Cog6ToothIcon, FireIcon } from "@heroicons/react/24/outline"
+import {
+  Cog6ToothIcon,
+  FireIcon,
+  MoonIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline"
 import { usePbAuth } from "@/contexts/AuthWrapper"
 import BottomBar from "@/components/BottomBar"
 import HeadBar from "@/components/HeadBar"
